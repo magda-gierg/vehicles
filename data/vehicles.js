0164ca9e0084b8f839c8b2ce67fdafeb2dd55507
magda-gierg <mjgiergiczny@gmail.com>
2017-07-28 10:48:39 +1200

    Update vehicles.js

--- data/vehicles.js
+++ data/vehicles.js
@@ -2,25 +2,42 @@ const vehicles = {
   cars: [
     {
       name: 'petrol',
-      description: 'BMW'
+      description: 'Porsche',
+      image: 'https://www.flatout.com.br/wp-content/uploads/2014/09/PorscheBrasil-001.jpg'
     },
     {
       name: 'electric',
-      description: 'Tesla'
+      description: 'BMW',
+      image: 'http://cdn1.autoexpress.co.uk/sites/autoexpressuk/files/styles/insert_main_image/public/2016/04/p90216947_highres.jpg?itok=FFVjKarM'
     },
     {
       name: 'hybrid',
-      description: 'Toyota Prius'
+      description: 'Toyota Prius',
+      image: 'http://i.telegraph.co.uk/multimedia/archive/02528/Toyota-Prius-Plugi_2528049k.jpg'
     }
   ],
   bikes: [
     {
       name: 'electric',
-      description: 'BULLS Lacuba EVO E8'
+      description: 'M55 Beast',
+      image: 'http://www.electric-bicycle-guide.com/images/M55-Beast.jpg'
     },
     {
       name: 'push-bike',
-      description: 'Trek'
+      description: 'Panama',
+      image: 'https://i5.walmartimages.com/asr/56647e0c-980f-4ad6-af44-0edabb4ce4d5_1.bfaaf3b4f0b61e9e7a3fb26cd0cf581e.jpeg'
+    }
+  ],
+  motorbike: [
+    {
+      name: 'sports bikes',
+      description: 'Honda',
+      image: 'https://s-media-cache-ak0.pinimg.com/originals/55/ae/93/55ae93a137cc0818d65a60a80cdf4edf.jpg'
+    },
+    {
+      name: 'cruisers',
+      description: 'Harley Davidson',
+      image: 'http://www.bikerperformance.com/images/products/akrapovic-slip-on-mufflers-harley-davidson-dyna-06-12-clone-78499.png?width=800'
     }
   ]
 }
